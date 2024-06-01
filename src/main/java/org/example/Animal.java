7c921eef0180a0627f7849769d275190e485cd8b
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-06-01 15:03:48 -0500

    Se realiza la evaluación

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,10 +2,14 @@ package org.example;
 
 public class Animal {
 
-    String nombre;
-    String genero
-    double peso=true;
-    Animal pareja=new;
+    private String nombre;
+    private String genero;
+    private Double peso;
+
+    public Animal() {
+    }
+
+    private Animal pareja;
 
 
     public Animal(String nombre, double peso) {
@@ -18,8 +22,35 @@ public class Animal {
     }
 
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getGenero() {
+        return genero;
+    }
 
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Animal getPareja() {
+        return pareja;
+    }
+
+    public void setPareja(Animal pareja) {
+        this.pareja = pareja;
+    }
 }
---
+++ src/main/java/org/example/Readme.md
@@ -0,0 +1,10 @@
+1. R// Tiene 3 clases
+2. R// Zoologico
+3. R// 2 objetos
+4. R// 4 atributos
+5. R// Ninguno
+6. R// No encuentra pareja y no crea familia
+7. R// 3 metodos 
+8. R// 1, recibe un nombre 
+9. R//2, al papa y a la mamá 
+10. R// vacío,  no retorma.
